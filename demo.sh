5501650a0c02dc25f7f4cf18155bc5017cddc62c
Eric Chabert <Eric.Chabert@cern.ch>
2018-09-10 17:49:05 +0200

    fix path issue

--- demo.sh
+++ demo.sh
@@ -4,8 +4,7 @@
 source /afs/cern.ch/user/e/echabert/.bashrc
 #coredumpsize limit (kbytes)
 ulimit -c 0
-cd /afs/cern.ch/user/e/echabert/CalibTreeAna/StoN
-cd CMSSW_10_1_0/src
+cd /afs/cern.ch/user/e/echabert/CalibTreeAna/StoN/CMSSW_10_1_0/src
 eval `scram runtime -sh`
-cd -
+cd /afs/cern.ch/user/e/echabert/CalibTreeAna/SiStripAna
 python StoN_producer.py OPTIONS
